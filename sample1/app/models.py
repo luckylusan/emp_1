8e8e55c45379da65a9eb578d870763936365620f
luckylusan <82433492+luckylusan@users.noreply.github.com>
2021-04-17 16:05:10 +0530

    Update models.py

--- sample1/app/models.py
+++ sample1/app/models.py
@@ -6,7 +6,7 @@ class employee(models.Model):
     emp_id = models.IntegerField()
     emp_name = models.CharField(max_length=200)
     emp_designation = models.CharField(max_length=200)
-    emp_doj = models.DateField()
+#     emp_doj = models.DateField()
 
     class Meta:
         db_table = "employee"
